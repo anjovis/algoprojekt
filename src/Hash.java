4a8b4f25ade6b1dadca2424fd64bfb92a92d3b66
Unknown <rebecka_engstrom@hotmail.com>
2018-05-08 16:45:42 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/Hash.java
+++ src/Hash.java
@@ -1,17 +1,42 @@
 
 public class Hash {
 	
-	private String[] hashArray;
+	private static String[] hashArray;
 	
 	public Hash() {
-		hashArray =  new String[10];
+		hashArray =  new String[199999];
 	}
 	
-	public int hashCode(String o) {
+	public static int hashCode(String o) {
+		
+		int half = o.length()/2;
+		int ascii =0;
+		int ascii2 =0;
+		String hashVal;
+
+		int result;
+		for(int i =0;i<half;i++) {
+			ascii = ascii + o.charAt(i);	
+		}
+		for(int i =half;i<o.length();i++) {
+			ascii2 = ascii2+o.charAt(i);	
+			
+		}
+		hashVal = Integer.toString(ascii)+Integer.toString(ascii2);
+		
+		result = Integer.parseInt(hashVal);
+		return result;
+		
+	}
+	
+	public static void main(String[] args) {
+		Hash hej = new Hash();
+		
+		System.out.println(hashCode("hej"));
+		System.out.println(hashCode("0"));
 		
 		
 		
-		return 4;
 		
 	}
 	
