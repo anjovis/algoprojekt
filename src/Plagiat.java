d0ed7ed2fd67ae78152aa35eb0a1faf34630b086
Unknown <anjkar17@student.hh.se>
2018-05-08 15:20:00 +0200

    igen

--- src/Plagiat.java
+++ src/Plagiat.java
@@ -6,48 +6,29 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Plagiat {
-	
-<<<<<<< HEAD
+
 
 	public static Hash readFile(String filnamn) {
-=======
-	
-	
-	public static void main (String [] args) {
->>>>>>> master
+
 		
 		File fil = new File(filnamn);
 		Hash hfil = new Hash();
 		
 		try {
-			
-<<<<<<< HEAD
+
 			Scanner sc = new Scanner(fil);
 			
 			while(sc.hasNext()) {
 				String ord = sc.next();
 				hfil.add(ord,hashCode(ord));
 			}
-	
-=======
-			String filnamn1 = "";
-			String filnamn2 = "";
-			
-			fr = new FileReader(filnamn1);
-			br = new BufferedReader(fr);
 
-			
->>>>>>> master
 		}
 		catch(FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		
-		
-<<<<<<< HEAD
-=======
-		
->>>>>>> master
+
 	}
 	
 	
