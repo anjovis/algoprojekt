4bc3f1ba82648389eb2f274a5eb7ab4f5a1bb67d
Unknown <rebecka_engstrom@hotmail.com>
2018-05-09 16:11:24 +0200

    den som ska användas

--- src/Hash.java
+++ src/Hash.java
@@ -2,34 +2,27 @@
 public class Hash {
 
 
-	private static String[][] hashArray;
-    private int value;
+	private static String[] hashArray;
+    private static int[] hashArrInt;
      
 	public Hash() {
-		hashArray =  new String[10007][];
-
+		hashArray =  new String[10007];
+		hashArrInt = new int[10007];
 	}
 	
 	public static int hashC(String o) {		
 		int a = o.hashCode();
 		int b = a % hashArray.length;		
 		return b;
-<<<<<<< HEAD
 		
 	}
 	
-	
-	//om arrayen �r full dubblera, kalla p� hashcode och l�gg in p� hascode (index) platsen .  
 	public void add(String o,int value){
 		String hej = hashArray[value];
 		if(o.compareTo(hashArray[value])==0) {
 			
-			
 		}
-		
-=======
->>>>>>> master
-		
+
 	}
 	
 	public int size(){
@@ -41,34 +34,7 @@ public class Hash {
 		Hash hej = new Hash();		
 		System.out.println(hashC("hej"));
 		System.out.println(hashC("0"));
-<<<<<<< HEAD
-		
-		
-		
-=======
 
-	public int hashC(String o) {
-		
-		hej.contains("hej");
-		
-		
+
 	}
-
-	public void add(String word, int value){
-
-		
-	}
-	public boolean contains(String word){
-
-   
-		
-	}
-	
-	public int size(){
-	
-		return hashArray.length;
-	}
-	
-	
-
 }
