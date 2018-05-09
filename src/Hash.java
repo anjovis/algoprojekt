1eb2e2c2b801173dec007fc4001f7c3c90b6ddc4
Unknown <rebecka_engstrom@hotmail.com>
2018-05-09 16:03:12 +0200

    ändrst lite

--- src/Hash.java
+++ src/Hash.java
@@ -2,12 +2,12 @@
 public class Hash {
 
 
-	private static String[][] hashArray;
-    private int value;
-     
+	private static String[] hashArray;
+	private static int[] hashArrInt;
 	
 	public Hash() {
-		hashArray =  new String[10007][];
+		hashArray =  new String[10007];
+		hashArrInt = new int[10007];
 
 	}
 	
@@ -20,6 +20,23 @@ public class Hash {
 		
 	}
 	
+	
+	//om arrayen �r full dubblera, kalla p� hashcode och l�gg in p� hascode (index) platsen .  
+	public void add(String o,int value){
+		String hej = hashArray[value];
+		if(o.compareTo(hashArray[value])==0) {
+			
+			
+		}
+		
+		
+	}
+	
+	public int size(){
+	
+		return hashArray.length;
+	}
+	
 	public static void main(String[] args) {
 		Hash hej = new Hash();
 		
@@ -29,17 +46,6 @@ public class Hash {
 		
 		
 		
-	}
-	//om arrayen �r full dubblera, kalla p� hashcode och l�gg in p� hascode (index) platsen .  
-	public void add(int value){
-
-   
-		
-	}
-	
-	public int size(){
-	
-		return hashArray.length;
 	}
 	
 	
