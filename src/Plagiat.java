c91d039b88351b4a49cdc502dd2bf67371517fde
Unknown <anjkar17@student.hh.se>
2018-05-17 09:40:22 +0200

    behöver ändras i hashTest

--- src/Plagiat.java
+++ src/Plagiat.java
@@ -3,8 +3,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Plagiat {
