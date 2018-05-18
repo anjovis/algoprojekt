734d0ad3cd9c2f89c594ebe75f9d86e53c3506a4
Unknown <rebecka_engstrom@hotmail.com>
2018-05-18 16:40:40 +0200

    Det slutgilitliga som jag skickade in

--- src/Hash.java
+++ src/Hash.java
@@ -11,6 +11,11 @@ public class Hash {
 		size = 0;
 	}
 
+	/**
+	 * hashC tar in en string och gör om till en hashcode.
+	 * @param String o
+	 * @return int b
+	 */
 	public int hashC(String o) {		
 		int a = o.hashCode();
 		int b = a % hashArray.length;
@@ -21,6 +26,11 @@ public class Hash {
 
 	}
 
+	/**
+	 * add tar in en string och addar stringen till hasharrayen pÂ den indexen som ordet fick av hashC
+	 * om platsen är upptagen med ett annat ord hoppar den till nästa plats och addar där istället.
+	 * @param String o
+	 */
 	public void add(String o){
 		if(o != null){
 			int value = hashC(o);
@@ -40,9 +50,11 @@ public class Hash {
 				}
 			}
 		}
-
-
 	}
+	
+	/**
+	 * print skriver ut hasharrayen och dess innehÂll och hoppar över alla platser som är null.
+	 */
 	public void print() {
 		for(int i =0;i<hashArray.length;i++) {
 			if(hashArray[i] !=null) {
@@ -53,6 +65,11 @@ public class Hash {
 		}
 	}
 
+	/**
+	 * find tar in en string och letar upp ordet på den indexplatsen och retunerar indexvärdet.
+	 * @param String word
+	 * @return int i
+	 */
 	public int find(String word){
 		if(word == null){
 			return -1;
@@ -69,7 +86,12 @@ public class Hash {
 		}
 		return -1;
 	}
-
+	
+	/**
+	 * getWord tar in en int i (index) och letar upp ordet som ligger på den platsen och retunerar ordet.
+	 * @param int i
+	 * @return String ord
+	 */
 	public String getWord(int i){
 		if(i < 0){
 			return null;
@@ -78,6 +100,11 @@ public class Hash {
 		return ord;
 	}
 
+	/**
+	 * getInt tar in en int i (index) och letar upp ordet som ligger på den platsen och retunerar frekvensen av ordet.
+	 * @param int i
+	 * @return int in
+	 */
 	public int getInt(int i){
 		if(i < 0){
 			return -1;
@@ -85,7 +112,11 @@ public class Hash {
 		int in = hashArrInt[i];
 		return in;
 	}
-
+	
+	/**
+	 * size läser av hur många platser som har ett ord i hasharrayen och retunerar så många platser som är upptagna.
+	 * @return int size
+	 */
 	public int size(){	
 		return size;
 	}
